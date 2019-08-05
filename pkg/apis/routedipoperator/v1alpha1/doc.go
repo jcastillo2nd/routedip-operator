b21a2afe45bc1d4f94ab193dff6b53cc8022ce9d
Javier Castillo II <j.castillo.2nd@jc2nd.me>
2019-08-04 22:34:39 -0500

    Deployment namespacing and Group consolidation
    - Namespaced to routed-ip-operator
    - Consolidated all Group references to routedipoperator

--- pkg/apis/routedipoperator/v1alpha1/doc.go
+++ pkg/apis/routedipoperator/v1alpha1/doc.go
@@ -1,4 +1,4 @@
 // Package v1alpha1 contains API Schema definitions for the routed-ip-operator v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=routed-ip-operator.jcastillo2nd.com
+// +groupName=routedipoperator.jcastillo2nd.com
 package v1alpha1
